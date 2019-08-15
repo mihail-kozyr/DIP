7dfcafa99c8a271ee1b34958db175d895e7eeba2
MihailKozyr <kozyr@rdtex.ru>
2019-08-15 15:46:56 +0300

    Chapter 2!

--- apihelper.py
+++ apihelper.py
@@ -2,4 +2,13 @@ def help(object, spacing=10, collapse=1):
     """Print methods and doc string.
 
     Takes module, class, list, dictionary, or string."""
-    methodList = [method for method]
+    methodList = [method for method in dir(object) if callable(getattr(object, method))]
+    processFunc = collapse and (lambda s: " ".join(s.split())) or (lambda s: s)
+    print("\n".join(["%s %s" %
+          (method.ljust(spacing),
+           processFunc(str(getattr(object, method).__doc__)))
+        for method in methodList
+        ]))
+
+if __name__ == '__main__':
+    print(help.__doc__)
