a960ac9b4a6e7d8e4969e89cbb901b6a07207f5b
MihailKozyr <kozyr@rdtex.ru>
2019-08-16 15:59:16 +0300

    Chapter 2!

--- apihelper.py
+++ apihelper.py
@@ -6,7 +6,7 @@ def help(object, spacing=10, collapse=1):
     processFunc = collapse and (lambda s: " ".join(s.split())) or (lambda s: s)
     print("\n".join(["%s %s" %
           (method.ljust(spacing),
-           processFunc(str(getattr(object, method).__doc__)))
+           processFunc(strl(getattr(object, method).__doc__)))
         for method in methodList
         ]))
 
